97a81bd439a590fa3c117c491b8b4c455ff143ca
= <=>
2018-02-15 13:12:38 +0000

    bugfix - fixed a problem where inactive and finished tasks were being shown to the user to log on

--- resources/views/outside/logging/loop_templates/project_accordion.blade.php
+++ resources/views/outside/logging/loop_templates/project_accordion.blade.php
@@ -6,7 +6,7 @@
 
    	<div class="row accordionhidden" id="{{{ 'task-accordion-folds-for-project-id-'.$project->id }}}">
 		@foreach($project->tasks as $task)
-			@if(!$task->task_finished && in_array($task->skill_id,$user->skills->pluck('id')->toArray()))
+			@if(!$task->task_finished && in_array($task->skill_id,$user->skills->pluck('id')->toArray()) && $task->task_active == 1 && $task->task_finished == 0 )
 				@include('outside.logging.loop_templates.task_rows', array('task'=> $task,'user'=>$user))
 			@endif
 		@endforeach			
