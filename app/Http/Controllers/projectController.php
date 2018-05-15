693a882324bc57fcb1e030c959b5a7dbf29d666d
Matt Hartley <matt.v.hartley@gmail.com>
2018-05-15 09:50:42 +0100

    add function to add default tasks to pre-existing project

--- app/Http/Controllers/projectController.php
+++ app/Http/Controllers/projectController.php
@@ -222,4 +222,87 @@ class projectController extends Controller
 		$project->save();
 
 	}
+
+
+
+	public function addTasksToProject($id){
+
+
+		$tasks = [];
+		$tasks[]=[
+			'Name'	=>	'First Stage Fitout',
+			'skill'	=>	3,
+			'desc'	=>	'Everything up to and including sprayfoam. Cleaning shell, bitumen, battens, floors, ballast and cutting back spray foam',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Exterior Paintwork',
+			'skill'	=>	4,
+			'desc'	=>	'Prep and Paint',
+		];
+
+
+		$tasks[]=[
+			'Name'	=>	'Interior Paint and Varnish',
+			'skill'	=>	5,
+			'desc'	=>	'Both furniture and boards',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Electrics',
+			'skill'	=>	6,
+			'desc'	=>	'All electrical Systems',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Water, Heating and Gas',
+			'skill'	=>	7,
+			'desc'	=>	'All Plumbing',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Engine and Mechanical Systems',
+			'skill'	=>	8,
+			'desc'	=>	'All systems',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Second Stage Fitout',
+			'skill'	=>	9,
+			'desc'	=>	'Fitting panels and bulkheads',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Final Fitout',
+			'skill'	=>	10,
+			'desc'	=>	'Fitting furniture and trimming',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Boat Setup',
+			'skill'	=>	11,
+			'desc'	=>	'Moving and leveling the boat',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Exterior Fittings',
+			'skill'	=>	12,
+			'desc'	=>	'Fender eyes, windows, bollards etc',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Warranty Work',
+			'skill'	=>	13,
+			'desc'	=>	'any rework required',
+		];
+
+		foreach($tasks as $task){
+			$loopTask = new tasks;
+			$loopTask->name = $task['Name'];
+			$loopTask->skill_id = $task['skill'];
+			$loopTask->description = $task['desc'];
+			$loopTask->project_id = $id;
+			$loopTask->save();
+		}
+	}
 }
