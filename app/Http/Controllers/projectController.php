d86948a52638309f1afd051a5f9caff35aa1217f
Matt Hartley <matt.v.hartley@gmail.com>
2018-05-15 09:48:09 +0100

    add create new project function  -auto fills from default hardcoded tasks

--- app/Http/Controllers/projectController.php
+++ app/Http/Controllers/projectController.php
@@ -111,4 +111,115 @@ class projectController extends Controller
 			$project->save();
 		}
 	}
+
+
+
+
+	public function newDefaultProject($name){
+
+
+		$project = new \App\project;
+
+		$project->name = $name;
+		$project->is_finished = 0;
+		$project->can_book_parts_to = 1;	
+		$project->can_log_hours = 1;	
+		$project->default_task = 0;	
+		$project->description = 'Description of project called '.$name ;			
+		$project->save();
+
+
+		$tasks = [];
+
+		$tasks[]=[
+			'Name'	=>	'First Stage Fitout',
+			'skill'	=>	3,
+			'desc'	=>	'Everything up to and including sprayfoam. Cleaning shell, bitumen, battens, floors, ballast and cutting back spray foam',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Exterior Paintwork',
+			'skill'	=>	4,
+			'desc'	=>	'Prep and Paint',
+		];
+
+
+		$tasks[]=[
+			'Name'	=>	'Interior Paint and Varnish',
+			'skill'	=>	5,
+			'desc'	=>	'Both furniture and boards',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Electrics',
+			'skill'	=>	6,
+			'desc'	=>	'All electrical Systems',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Water, Heating and Gas',
+			'skill'	=>	7,
+			'desc'	=>	'All Plumbing',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Engine and Mechanical Systems',
+			'skill'	=>	8,
+			'desc'	=>	'All systems',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Second Stage Fitout',
+			'skill'	=>	9,
+			'desc'	=>	'Fitting panels and bulkheads',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Final Fitout',
+			'skill'	=>	10,
+			'desc'	=>	'Fitting furniture and trimming',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Boat Setup',
+			'skill'	=>	11,
+			'desc'	=>	'Moving and leveling the boat',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Exterior Fittings',
+			'skill'	=>	12,
+			'desc'	=>	'Fender eyes, windows, bollards etc',
+		];
+
+		$tasks[]=[
+			'Name'	=>	'Warranty Work',
+			'skill'	=>	13,
+			'desc'	=>	'any rework required',
+		];
+
+
+
+			
+		$defaultTask = new tasks;
+		$defaultTask->name = 'Default Task';
+		$defaultTask->skill_id = 2;
+		$defaultTask->project_id = $project->id;
+		$defaultTask->description = 'Default task for this project';
+		$defaultTask->save();
+		$project->default_task = $defaultTask->id;
+
+		foreach($tasks as $task){
+			$loopTask = new tasks;
+			$loopTask->name = $task['Name'];
+			$loopTask->skill_id = $task['skill'];
+			$loopTask->description = $task['desc'];
+			$loopTask->project_id = $project->id;
+			$loopTask->save();
+		}
+
+		$project->is_boat = 0;
+		$project->save();
+
+	}
 }
